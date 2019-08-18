5a1e3148e930e785875c1b6060411cdfb3b5a227
Joren Paps <joren.paps@spikes.be>
2019-08-18 20:07:57 +0200

    qsdfq

--- src/app/services/datastore.service.ts
+++ src/app/services/datastore.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import * as nedb from 'nedb';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatastoreService {
+  private basePath = './db/';
+  private db = {};
+
   constructor() {}
+
+  private getCollection(name: string): nedb {
+    if (this.db[name] === undefined) {
+      this.db[name] = new nedb({
+        filename: `${this.basePath}${name}.json`,
+        autoload: true
+      });
+    }
+
+    return this.db[name];
+  }
 }
